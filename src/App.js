e15983ddfc200f022dbc417befc2f59fdf0b5c7b
German Velibekov <german.velibekov@gmail.com>
2020-05-10 08:59:45 +0300

    static data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
 import './App.css';
+import 'tachyons'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='tc'>
+      <header>Are things getting any better in US?</header>
+      <h1>YES</h1>
+      <h2>Aren't all the information and news regarding COVID-19 situation make you anxious and probably sad
+      while the only information you are looking for is if the situation has improved or not?
+      Then this site is for you! It shows yes/no answer to the main question of today.
+        </h2>
+      <footer>
+        For more information check <a href='https://www.worldometers.info/coronavirus/'>Corona Virus Updates</a>
+      </footer>
     </div>
   );
 }
