8d48618eb3b67c02ab3461664de76b54b1294c7f
German Velibekov <german.velibekov@gmail.com>
2020-05-12 11:16:40 +0300

    styling loading indicator

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Loading from './Loading'
 
 function Answer({ isBetter }) {
-  return <Loading />
   if (typeof isBetter === 'undefined') { return <Loading /> }
   if (isBetter) {
     return <h1>YES</h1>
--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,7 +2,14 @@ import React from 'react'
 import ReactLoading from 'react-loading'
 
 function Loading() {
-  return <ReactLoading type='bars' color='#555555' className='center' />
+  return (
+    <ReactLoading
+      type='spinningBubbles'
+      color='#555555'
+      className='center mv3'
+      height='32px'
+      width='32px' />
+  )
 }
 
 export default Loading;
