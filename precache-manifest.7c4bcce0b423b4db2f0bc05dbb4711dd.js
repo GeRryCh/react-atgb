bdd24aa7e3c0dcde416b5824d1772620a7db584f
German Velibekov <german.velibekov@gmail.com>
2020-05-14 15:30:00 +0300

    Updates

---
+++ precache-manifest.7c4bcce0b423b4db2f0bc05dbb4711dd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d4e84a0d092d9164d03bff11cee5ed9",
+    "url": "/react-atgb/index.html"
+  },
+  {
+    "revision": "c750da033de4292294d1",
+    "url": "/react-atgb/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3a6ce3d5e4521ea9d554",
+    "url": "/react-atgb/static/css/main.283650ba.chunk.css"
+  },
+  {
+    "revision": "c750da033de4292294d1",
+    "url": "/react-atgb/static/js/2.c0b92194.chunk.js"
+  },
+  {
+    "revision": "c3f4a91b08326551c3d2c23a3296dfa8",
+    "url": "/react-atgb/static/js/2.c0b92194.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a6ce3d5e4521ea9d554",
+    "url": "/react-atgb/static/js/main.53ed1bc0.chunk.js"
+  },
+  {
+    "revision": "35047f511b15d93e7a00",
+    "url": "/react-atgb/static/js/runtime-main.539162d1.js"
+  },
+  {
+    "revision": "224d2c423a6828ed0a93fbd6eaee719f",
+    "url": "/react-atgb/static/media/samsungsharpsans-medium.224d2c42.woff"
+  }
+]);
